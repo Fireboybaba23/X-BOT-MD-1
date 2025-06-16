60c39c0ed3e1364bb2c30fd27fc3c0444d234b51
Fireboybaba23 <fireboyyoung6@gmail.com>
2025-06-16 10:00:50 +0100

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "XBOT-MD**BJH0NGCQFABXWPE",
+SESSION_ID: process.env.SESSION_ID || "XBOT-MD**BDW5K9LG5P0ME7M",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
@@ -34,7 +34,7 @@ PREFIX: getConfig("PREFIX") || ".",
 // add your prifix for bot   
 BOT_NAME: process.env.BOT_NAME || "X-BOT-MD",
 // add bot namw here for menu
-STICKER_NAME: process.env.STICKER_NAME || "X-BOT-MD",
+STICKER_NAME: process.env.STICKER_NAME || "Him",
     
 VPS: process.env.VPS || "",
 // type sticker pack name 
